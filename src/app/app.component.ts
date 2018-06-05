f246c920ee343f68bff946bb70d90a3a63cfd534
Jonathan <jtecktips@gmail.com>
2018-06-05 16:56:10 -0700

    Added notes to help figure the assignment out

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+
+  // declare two number
+
+  // create another variable thats the sum
+
+  // create a method getLargest() that returns the largest value of an array of numbers...
+
+  // use {{ }} to bind to the result i.e. {{ getLargest([num1, num2, etc...]) }}
 }
